a35ad8fbaa5c201b8de151be77dfd5c9550a6b11
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-03-05 19:28:45 +0600

    variable rules test

--- 02_variable/main.go
+++ 02_variable/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+//package level vriable
+var i int = 25
 
 func main() {
-	fmt.Println("Nihad")
+	fmt.Println(i)
+	//redeclare the package level variable
+	//note:defferent scope declare same name variable
+	i := 27
+	fmt.Println(i)
+	//shadow this variable
+	i = 52
+	fmt.Println(i)
+
+	//====================================================================================================
+	//type conversion
+	var j float64
+	j = float64(i)
+	fmt.Printf("%v %T \n", j, j)
+	//when conversion int to string use strconv library as don't risk to value lose in compiler
+	s := strconv.Itoa(i)
+	fmt.Printf("%v %T \n", s, s)
+
 }
